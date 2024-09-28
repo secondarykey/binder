7e1f849fb9bb96768eb3c88d6cfc9fa8542e9862
secondarykey <secondarykey@gmail.com>
2024-09-28 16:03:02 +0900

    fix: Adding config update commit

--- config.go
+++ config.go
@@ -2,26 +2,28 @@ package binder
 
 import (
 	"binder/db/model"
+	"binder/fs"
 
 	"golang.org/x/xerrors"
 )
 
 func (b *Binder) EditConfig(conf *model.Config) error {
+
 	if b == nil {
 		return EmptyError
 	}
-	org, err := b.db.GetConfig()
-	if err != nil {
-		return xerrors.Errorf("db.GetConfig() error: %w", err)
-	}
-	conf.Created = org.Created
 
-	err = b.db.UpdateConfig(conf, b.op)
+	err := b.db.UpdateConfig(conf, b.op)
 	if err != nil {
 		return xerrors.Errorf("db.UpdateConfig() error: %w", err)
 	}
 
-	//TODO Binder コミット
+	fn := fs.ConfigTableFile()
+	err = b.fileSystem.Commit(fs.M("Update Config", "Main"), fn)
+	if err != nil {
+		return xerrors.Errorf("Commit() error: %w", err)
+	}
+
 	return nil
 }
 
