6910c69664bb75bd2b56d45e5507e4157dc30af8
secondarykey <secondarykey@gmail.com>
2025-02-18 19:06:26 +0900

    fix: markdown double buffering

--- _cmd/binder/frontend/src/components/HTMLFrame.jsx
+++ _cmd/binder/frontend/src/components/HTMLFrame.jsx
@@ -10,82 +10,116 @@ import Message from '../Message';
  */
 class HTMLFrame extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.components = [];
-        this.components.push(React.createRef());
-        this.components.push(React.createRef());
+  constructor(props) {
+    super(props);
 
-        this.current = undefined;
+    this.components = [];
+    this.components.push(React.createRef());
+    this.components.push(React.createRef());
+    this.number = 1;
+
+    this.current = undefined;
+
+    this.interval = -1;
+
+    this.view = this.view.bind(this);
+  }
+
+  componentDidMount() {
+    this.view();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.html != this.props.html) {
+      return true;
+    }
+    return false;
+  }
+
+  componentDidUpdate() {
+    this.view();
+    /*
+    var sec = 0.3
+    if ( this.interval > 0 ) {
+      clearTimeout(this.interval);
+      this.interval = setTimeout(this.view,sec * 1000);
+    } else {
+      this.interval = setTimeout(function() {},sec * 1000);
+      this.view();
+    }
+    */
+  }
+
+  view() {
+
+    var html = this.props.html;
+
+    var current = this.components[0];
+    var hide = this.components[1];
+    if (current === this.current) {
+      current = this.components[1];
+      hide = this.components[0];
+      this.number = 2;
+    } else {
+      this.number = 1;
     }
 
-    componentDidMount() {
-        this.view(this.props.html);
-    }
+    var c = current.current;
+    var h = hide.current;
 
-    componentDidUpdate() {
-        this.view(this.props.html);
-    }
+    //新しく表示する側にHTMLを設定
+    c.srcdoc = html
 
-    view(html) {
+    //表示箇所を真ん中にする
+    setTimeout(function () {
 
-        var current = this.components[0];
-        var hide = this.components[1];
+      var doc = c.contentDocument || c.contentWindow.document;
+      //クリック禁止を行う
+      doc.addEventListener("click", function (e) {
+        e.preventDefault();
+      });
 
-        if ( current === this.current ) {
-            current = this.components[1];
-            hide = this.components[0];
-        }
+      var f = doc.querySelector("#binder_focus_id");
+      var diagrams = doc.querySelectorAll("div.binderSVG");
 
-        var c = current.current;
-        var h = hide.current;
-
-        //新しく表示する側にHTMLを設定
-        c.srcdoc = html
-
-        //表示箇所を真ん中にする
-        setTimeout(function() {
-
-          var doc = c.contentDocument || c.contentWindow.document;
-    
-          //クリック禁止を行う
-          doc.addEventListener("click",function(e) {
-            e.preventDefault();
+      if (diagrams !== null) {
+        //ノート内の描画について
+        diagrams.forEach((elm) => {
+          var txt = elm.textContent;
+          Mermaid.parse(txt).then((data) => {
+            elm.innerHTML = data.svg;
+          }).catch((err) => {
+            Message.showWarning("Diagram parse error:" + err);
           });
+        })
+      }
 
-          var f = doc.querySelector("#binder_focus_id");
-          var diagrams = doc.querySelectorAll("div.binderSVG");
+      if (f !== undefined && f !== null) {
+        f.scrollIntoView({ behavior: 'instant', block: "center" })
 
-          if ( diagrams !== null  ) {
-            //ノート内の描画について
-            diagrams.forEach( (elm) => {
-                var txt = elm.textContent;
-                Mermaid.parse(txt).then( (data) => {
-                    elm.innerHTML = data.svg;
-                }).catch ( (err) => {
-                  Message.showWarning("Diagram parse error:" + err);
-                });
-            })
-          }
+      }
 
-          if ( f !== undefined && f !== null ) {
-            f.scrollIntoView({  behavior: 'instant', block:"center" })
-          }
+      h.classList.remove("show");
+      h.classList.add("hide");
 
-          h.style.opacity = 0.0;
-          c.style.opacity = 1.0;
-        } , 10);
+      c.classList.remove("hide");
+      c.classList.add("show");
 
-        //現在の表示側を設定
-        this.current = current;
-    }
+      this.interval = -1;
 
-    render() {
-        return (<>
-            <iframe className="htmlViewer" ref={this.components[0]}></iframe>
-            <iframe className="htmlViewer" ref={this.components[1]}></iframe>
-        </>)
-    }
+    }, 100);
+
+    //現在の表示側を設定
+    this.current = current;
+  }
+
+  render() {
+    return (<>
+      <div id="bufferd"> {this.number} </div>
+      <iframe className="htmlViewer" ref={this.components[0]}></iframe>
+      <iframe className="htmlViewer" ref={this.components[1]}></iframe>
+    </>)
+  }
 }
 
 export default HTMLFrame;
