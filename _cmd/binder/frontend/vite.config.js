f6245e0133def0377f1d3995bb59bfdc17a7746d
secondarykey <secondarykey@gmail.com>
2026-03-20 05:42:30 +0900

    fix: 開発ビルド時のRollupワーニングを無視

--- _cmd/binder/frontend/vite.config.js
+++ _cmd/binder/frontend/vite.config.js
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
   optimizeDeps: {
     include: ['tree'],
   },
