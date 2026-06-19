e710d183ff57f8f2c84556210b73fa6a74b9a855
secondarykey <secondarykey@gmail.com>
2026-06-19 09:33:52 +0900

    Revert "fix: エディタ下部で Enter 改行時にカーソルが隠れる問題を修正"
    This reverts commit ed673b230f32d0a1bfcfea8a028f5995f89ddc77.

--- _cmd/shared/frontend/editor/EditorArea.jsx
+++ _cmd/shared/frontend/editor/EditorArea.jsx
@@ -156,9 +156,6 @@ function EditorArea({ text, style, showLineNumbers = true, wordWrap = true, acti
     const cs = window.getComputedStyle(textarea);
     const lineHeight = parseFloat(cs.lineHeight) || 20;
     const paddingTop = parseFloat(cs.paddingTop) || 0;
-    const paddingLeft = parseFloat(cs.paddingLeft) || 0;
-    const paddingRight = parseFloat(cs.paddingRight) || 0;
-    const contentWidth = textarea.clientWidth - paddingLeft - paddingRight;
 
     const mirror = document.createElement('div');
     const s = mirror.style;
@@ -166,7 +163,7 @@ function EditorArea({ text, style, showLineNumbers = true, wordWrap = true, acti
     s.top = '-9999px';
     s.left = '-9999px';
     s.visibility = 'hidden';
-    s.width = contentWidth + 'px';
+    s.width = textarea.clientWidth + 'px';
     s.height = 'auto';
     s.padding = '0';
     s.border = '0';
